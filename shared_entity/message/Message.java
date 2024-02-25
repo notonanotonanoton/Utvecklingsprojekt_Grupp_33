d9fef1568b4ea808ec39dfcaf0c889acaa861795
notonanotonanoton <anotona@protonmail.com>
2024-02-25 21:43:00 +0100

    added class diagram and made very minor adjustments

--- shared_entity/message/Message.java
+++ shared_entity/message/Message.java
@@ -7,21 +7,31 @@ import shared_entity.user.User;
 import javax.swing.*;
 import java.io.Serializable;
 import java.util.Date;
+import java.util.List;
 
 public class Message implements Serializable {
     private MessageState messageState;
     private User sender;
-    private User[] receivers;
+    private List<User> receivers;
     private String messageText;
     private ImageIcon messageImage;
     private Date receivedByServer;
-    private Date sentToClient;
+    private Date receivedByUser;
 
     public Message() {
         messageState = new MessageWaiting();
     }
     public Message(String messageText) {
+        this();
         this.messageText = messageText;
+    }
+    public Message(ImageIcon messageImage) {
+        this();
+        this.messageImage = messageImage;}
+    public Message(String messageText, ImageIcon messageImage) {
+        this();
+        this.messageText = messageText;
+        this.messageImage = messageImage;
     }
 
     public void setState(MessageState messageState) {
